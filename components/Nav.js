c877ea620418d3f1d170fb7e82e248f4af24042a
Rokas <rookas.rudzenskas@gmail.com>
2021-05-27 07:14:45 +0300

    Rokas

--- components/Nav.js
+++ components/Nav.js
@@ -3,13 +3,14 @@ import requests from "../utils/requests";
 
 const Nav = () => {
     return (
-        <nav>
-            <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll">
+        <nav className="relative">
+            <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
                 {Object.entries(requests).map(([key, { title, url }]) => (
 
                     <h2 key={key} className=" last:pr-24 cursor-pointer transition transform duration-100 hover:scale-125 hover:text-white">{title}</h2>
                 ))}
             </div>
+            <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
         </nav>
     );
 };
