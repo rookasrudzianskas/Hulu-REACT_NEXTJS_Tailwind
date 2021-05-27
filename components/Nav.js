4906b69bea8bb6b48ff9193c48f86f663ac5ae7e
Rokas <rookas.rudzenskas@gmail.com>
2021-05-27 07:17:49 +0300

    Rokas

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import requests from "../utils/requests";
+import { useRouter } from "next/router";
 
 const Nav = () => {
+    const router = useRouter();
+
     return (
         <nav className="relative">
             <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
                 {Object.entries(requests).map(([key, { title, url }]) => (
 
-                    <h2 key={key} className=" last:pr-24 cursor-pointer transition transform duration-100 hover:scale-125 hover:text-white">{title}</h2>
+                    <h2 onClick={() => router.push(`/?genre=${key}`)} key={key} className=" last:pr-24 cursor-pointer transition transform duration-100 hover:scale-125 hover:text-white">{title}</h2>
                 ))}
             </div>
             <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
