84d2db23e61e7589f25347214f9d9934a09476d6
Rokas <rookas.rudzenskas@gmail.com>
2021-05-27 08:15:44 +0300

    Rokas

--- components/Results.js
+++ components/Results.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import requests from "../utils/requests";
 import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
 
 const Results = ({results}) => {
     return (
-        <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+        <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
             {results.map(result => (
                 <Thumbnail key={result.id} result={result} />
             ))}
 
-        </div>
+        </FlipMove>
     );
 };
 
